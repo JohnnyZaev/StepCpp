56be029ca17b1f5e969aa3c36470b6f8c91cec5b
gvarys <johnnyzaev@gmail.com>
2023-01-11 13:10:03 +0600

    Array second lesson 5th task almost done!

--- Arrays/Array2.cpp
+++ Arrays/Array2.cpp
@@ -161,4 +161,12 @@ int main()
 		arr[i] = rand();
 		cout << arr[i] << ' ';
 	}
+
+	int result = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < 0)
+			result += arr[i];
+	}
+	cout << result;
 }
